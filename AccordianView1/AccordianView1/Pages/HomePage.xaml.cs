a9685d828291821135494d76cc32173c6231b480
XPANXIONMAIL\sarafs <sarafs@xpanxion.co.in>
2018-06-15 14:15:36 +0530

    Add project files.

---
+++ AccordianView1/AccordianView1/Pages/HomePage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AccordianView1.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HomePage : ContentPage
+	{
+		public HomePage ()
+		{
+			InitializeComponent ();
+		}
+        public void OnXamlClicked(object sender, EventArgs args)
+        {
+
+            Navigation.PushAsync(new XamlExample());
+
+        }
+        public void OnCodeClicked(object sender, EventArgs args)
+        {
+
+            Navigation.PushAsync(new CodeEaxmple());
+
+        }
+    }
+}
